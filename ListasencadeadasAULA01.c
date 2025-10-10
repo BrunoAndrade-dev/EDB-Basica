4423b712678ec5b88736ee3c5b396d91d9ef9124
Bruno Raphael <brunorafha31@gmail.com>
2025-10-10 20:36:38 -0300

    codigo quase concluido de listas encadeadas

--- ListasencadeadasAULA01.c
+++ ListasencadeadasAULA01.c
@@ -23,7 +23,7 @@ Lista insere (Lista*list , int valor){
 //Listando valores
 void listar (Lista*list) {
     if (isempty){
-        printf ("\n lista estÃ¡ vazia") ;
+        printf ("\n lista está vazia") ;
         return ;
     }
     Lista *aux = list ;
@@ -46,7 +46,7 @@ int buscar (Lista *list ,int v) {
     return 0 // Nao encontrado
 
 }
-//forma alternativa de implementar a funÃ§Ã£o busca utilizando for
+//forma alternativa de implementar a função busca utilizando for
 // Lista *busca (Lista*list , int v ){
     //Lista *aux ;
     //for (aux = list ; aux != NULL ; aux = aux -> next){
@@ -54,13 +54,42 @@ int buscar (Lista *list ,int v) {
            // return aux ;
         //}
     //}
-    //return  NULL ; se nÃ£o for encontrado
+    //return  NULL ; se não for encontrado
 //}
+
+Lista *retirar (Lista *list , int v) {
+    Lista ant = NULL ;
+    Lista *p = list; //list aponta para o primeiro nó da lista
+    while (p!=NULL && p->info != v){
+        ant = p;
+        p= p->next ;
+    }
+    if (p==NULL){
+        return list;
+    }
+    if (ant == NULL){
+        list = p->next;
+        free(p) ;
+    } else (ant -> next == p-> next) {
+       ant ->next = p-> next;
+    }
+
+    free(p);
+    return list;
+}
+//Liberar a lista
+Lista liberar(Lista *list){
+    Lista *p=list ;
+    while (p!=NULL){
+        list =p-> next ;
+        free(p);
+        p=list;
+    }
+}
 //Main
 int main(){
     Lista mylista = createList();
     mylista = insere(mylista , 10);
     mylista = insere(mylista , 3 );
     listar (mylista) ;
-    
-
+    return 0 ;
